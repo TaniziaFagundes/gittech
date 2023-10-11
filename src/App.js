ea42220a214b2e9ea00dca4206c67ab8d5ceea8e
TaniziaFagundes <tdlf@icomp.ufam.edu.br>
2023-10-11 16:43:27 -0400

    feat: add functions and test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+export const fahrenheitToCelsius = (fahrenheit) => {
+  return ((fahrenheit - 32) * 5) / 9;
+};
+
+export const celsiusToFahrenheit = (celsius) => {
+  return (celsius * 9) / 5 + 32;
+};
 
 function App() {
+  const [grausF, setGrausF] = useState();
+  const [grausC, setGrausC] = useState();
+  const [celsius, setCelsius] = useState();
+  const [fahrenheit, setFahrenheit] = useState();
+
+  const handleFahrenheitClick = () => {
+    const celsiusValue = fahrenheitToCelsius(grausF);
+    setCelsius(celsiusValue);
+  };
+
+  const handleCelsiusClick = () => {
+    const fahrenheitValue = celsiusToFahrenheit(grausC);
+    setFahrenheit(fahrenheitValue);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Estudando sobre Integração continua.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ display: "flex" }}>
+          <div>
+            <input value={grausF} onChange={(e) => setGrausF(e.target.value)} />
+            <button onClick={handleFahrenheitClick}>ver em celsius</button>
+            <p>{celsius && `${celsius} °C`}</p>
+          </div>
+          <div>
+            <input value={grausC} onChange={(e) => setGrausC(e.target.value)} />
+            <button onClick={handleCelsiusClick}>ver em fahrenheit</button>
+            <p>{fahrenheit && `${fahrenheit} °F`}</p>
+          </div>
+        </div>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
+import { fahrenheitToCelsius, celsiusToFahrenheit } from "./App";
+test("Should buttons", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const elementButton1 = screen.getByText(/ver em celsius/i);
+  const elementButton2 = screen.getByText(/ver em fahrenheit/i);
+  expect(elementButton1).toBeInTheDocument();
+  expect(elementButton2).toBeInTheDocument();
+});
+
+test("32°F is 0°C", () => {
+  expect(fahrenheitToCelsius(32)).toBe(0);
+});
+
+test("68°F is 20°C", () => {
+  expect(fahrenheitToCelsius(68)).toBe(20);
+});
+
+test("-40°F is -40°C", () => {
+  expect(fahrenheitToCelsius(-40)).toBe(-40);
+});
+
+test("212°F is 100°C", () => {
+  expect(fahrenheitToCelsius(212)).toBe(100);
+});
+
+test("0°C is 32°F", () => {
+  expect(celsiusToFahrenheit(0)).toBe(32);
+});
+
+test("20°C is 68°F", () => {
+  expect(celsiusToFahrenheit(20)).toBe(68);
+});
+
+test("-40°C is -40°F", () => {
+  expect(celsiusToFahrenheit(-40)).toBe(-40);
+});
+
+test("100°C is 212°F", () => {
+  expect(celsiusToFahrenheit(100)).toBe(212);
 });
